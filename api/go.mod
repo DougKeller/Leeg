6ce30724789440253c421baf9c45d32f50cd0b9d
DougKeller <dwk24@zips.uakron.edu>
2021-05-09 15:07:29 -0400

    setup basic riot call (kernel does not work atm)

--- api/go.mod
+++ api/go.mod
@@ -3,13 +3,16 @@ module leeg_api
 go 1.16
 
 require (
+	github.com/KnutZuidema/golio v0.0.0-20210209144508-671837199bbc // indirect
 	github.com/gin-gonic/gin v1.7.1 // indirect
 	github.com/go-playground/validator/v10 v10.6.0 // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/json-iterator/go v1.1.11 // indirect
+	github.com/konsorten/go-windows-terminal-sequences v1.0.3 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.1 // indirect
+	github.com/sirupsen/logrus v1.8.1 // indirect
 	github.com/ugorji/go v1.2.5 // indirect
 	golang.org/x/crypto v0.0.0-20210506145944-38f3c27a63bf // indirect
 	golang.org/x/sys v0.0.0-20210507161434-a76c4d0a0096 // indirect
--- api/src/main.go
+++ api/src/main.go
@@ -1,10 +1,20 @@
 package main
 
 import (
-	"net/http"
-	"github.com/gin-gonic/gin"
+  "os"
+  "log"
+  "net/http"
   "io/ioutil"
+  "github.com/gin-gonic/gin"
+  "github.com/KnutZuidema/golio"
+  "github.com/KnutZuidema/golio/api"
 )
+
+func GetGolioClient() *golio.Client {
+  client := golio.NewClient(os.Getenv("RIOT_API_KEY"),
+            golio.WithRegion(api.RegionNorthAmerica))
+  return client
+}
 
 func setupRouter() *gin.Engine {
 	router := gin.Default()
@@ -13,12 +23,21 @@ func setupRouter() *gin.Engine {
 		c.String(http.StatusOK, "Thanks for hitting this endpoint, fink")
 	})
 
-  router.GET("/get", func(c *gin.Context){
+  router.GET("/get", func(c *gin.Context) {
     client := &http.Client{}
     resp, _ := client.Get("https://httpbin.org/get")
     defer resp.Body.Close()
     body, _ := ioutil.ReadAll(resp.Body)
     c.String(http.StatusOK, string(body))
+  })
+
+  router.GET("/summoner/:name", func(c *gin.Context) {
+    client := GetGolioClient()
+    summoner, err := client.Riot.Summoner.GetByName(c.Param("name"))
+    if err != nil {
+      log.Print(err)
+    }
+    c.JSON(http.StatusOK, summoner)
   })
 
 	return router
